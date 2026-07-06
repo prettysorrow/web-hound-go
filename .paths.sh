ba029941ac6255ccf0c1dccd643e76a728885549
prettysorrow <prettysorrow44@gmail.com>
2026-07-07 01:10:08 +0300

    add env configuration files
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ .paths.sh
@@ -0,0 +1,13 @@
+export REPO_ROOT="/Users/psi/Desktop/go-demo"
+
+export POSTGRES_SERVICE="postgres-server"
+export POSTGRES_CONTAINER="go-postgres-server"
+export MIGRATE_SERVICE="postgres-migrate"
+export MIGRATE_CONTAINER="go-postgres-migrate"
+
+export POSTGRES_HOST_PATH="$REPO_ROOT/postgres-db"
+export POSTGRES_DOCKER_PATH="/var/lib/postgresql"
+export POSTGRES_CONNECTION="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres-server:5432/$POSTGRES_DB"
+
+export MIGRATE_HOST_PATH="$REPO_ROOT/postgres-migrations"
+export MIGRATE_DOCKER_PATH="/migrations"
