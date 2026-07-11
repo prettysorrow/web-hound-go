f3fa9edd97b5ed6bfa1467f2c5718293530796c5
prettysorrow <prettysorrow44@gmail.com>
2026-07-12 01:17:42 +0300

    add some DTOs
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ transport/inputs.go
@@ -0,0 +1,21 @@
+package webhound_transport
+
+type GetUserInput struct {
+	Id int64 `json:"id"`
+}
+
+type PostUserInput struct {
+	DisplayName string `json:"display_name"`
+	UsedService string `json:"used_service"`
+}
+
+type GetUsersInput struct {
+}
+
+type GetRequestInput struct {
+	Id int64 `json:"id"`
+}
+
+type GetRequestsByUserIdInput struct {
+	UserId string `json:"user_id"`
+}
