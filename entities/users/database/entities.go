ae063d0bc3ca52e756752dd886a761313e48bf73
prettysorrow <prettysorrow44@gmail.com>
2026-07-14 00:53:38 +0300

    change users representation
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- entities/users/database/entities.go
+++ entities/users/database/entities.go
@@ -1,7 +1,8 @@
-package webhound_users_models
+package webhound_users_database
 
 type User struct {
-	Id          int64  `json:"id" db:"id"`
+	Id          int64  `json:"id"           db:"id"`
 	DisplayName string `json:"display_name" db:"display_name"`
 	UsedService string `json:"used_service" db:"used_service"`
+	ServiceId   string `json:"service_id"   db:"service_id"`
 }
