fd62b66ef6cf0884dfde0787589e8e3c75bc6d73
prettysorrow <prettysorrow44@gmail.com>
2026-07-21 23:27:26 +0300

    fix some bugs and provide better validation for steam fetching service
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- fetching/steam/dto.py
+++ fetching/steam/dto.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
+from fastapi import HTTPException, status
 from utils.dtos import ForbidExtraModel
 from pydantic import Field, AnyUrl
 from typing import Optional, List
 
 
 class SteamUser(ForbidExtraModel):
-    user_slug: str = Field(max_length=100)
     user_url: AnyUrl
+    user_slug: Optional[str] = Field(max_length=100)
     display_name: str = Field(max_length=100)
     avatar_url: Optional[AnyUrl] = None
     friends: Optional[List[SteamUser]] = None
+
+
+UnexpectedSteamAPIResponseFormatException: HTTPException = HTTPException(
+    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    detail="unexpected steam api response format",
+)
--- fetching/steam/handlers.py
+++ fetching/steam/handlers.py
@@ -1,24 +1,54 @@
-import os
 import httpx
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from steam.auth import auth_params
-from steam.dto import SteamUser
+from steam.dto import SteamUser, UnexpectedSteamAPIResponseFormatException
+from typing import Optional, List
+from pydantic import AnyUrl
 
-router = APIRouter(prefix="/api/fetching/steam/users", tags=["fetching", "steam"])
+router = APIRouter(
+    prefix="/api/fetching/steam/users", tags=["fetching", "steam", "users"]
+)
 
 base_url = "https://api.steampowered.com"
 
 
+# uses resolve_vanity_url steam enpoint to find steam id by user vanity url (user slug)
 def get_steamid(slug: str):
-    params = auth_params + {"vanity_url": slug}
+    params = auth_params | {"vanityurl": slug}
     response = httpx.get(f"{base_url}/ISteamUser/ResolveVanityURL/v0001", params=params)
-    return response.json()["response"]["steamid"]
+    content = response.json()
+
+    ## begin response format validation
+    if response.status_code != 200:
+        raise UnexpectedSteamAPIResponseFormatException
+
+    if "response" not in content or "success" not in content["response"]:
+        raise UnexpectedSteamAPIResponseFormatException
+
+    if content["response"]["success"] != 1:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"failed to resolve steamid for user with slug={slug}",
+        )
+
+    if "steamid" not in content["response"]:
+        raise UnexpectedSteamAPIResponseFormatException
+    ## end response format validation
+
+    return content["response"]["steamid"]
 
 
-# expects something like "https://steamcommunity.com/id/prettysorrow/"
-def get_slug(user_url: str):
+# expects something like https://steampowered.com/profile/67696661377 or https://steampowered.com/id/prettysorrow
+def get_slug(user_url: AnyUrl) -> Optional[AnyUrl]:
     if "steamcommunity.com/profiles/" in user_url:
-        raise Exception("not supported")
+        # it means user has no slug
+        return None
+
+    if "steamcommunity.com/id/" not in user_url or len(user_url.split("/id/")) != 2:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="unexpected user_url format",
+        )
 
     return user_url.split("/id/")[-1].strip("/")
 
@@ -29,17 +59,55 @@ def get_user(slug: str):
 
     # get friends ids
     endpoint = f"{base_url}/ISteamUser/GetFriendList/v0001"
-    params = auth_params + {"steamid": steam_id} + {"relationship": "friend"}
+    params = auth_params | {"steamid": steam_id} | {"relationship": "friend"}
     response = httpx.get(endpoint, params=params)
+    content = response.json()
+    ## begin response format validation
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"failed to fetch friend list for user with slug={slug}: {content}",
+        )
+
+    if "friendslist" not in content or "friends" not in content["friendslist"]:
+        raise UnexpectedSteamAPIResponseFormatException
+
+    for friend in content["friendslist"]["friends"]:
+        if "steamid" not in friend:
+            raise UnexpectedSteamAPIResponseFormatException
+    ## end response format validation
     friends_steam_ids = [
         friend["steamid"] for friend in response.json()["friendslist"]["friends"]
     ]
 
     # get summaries
     endpoint = f"{base_url}/ISteamUser/GetPlayerSummaries/v0002"
-    params = auth_params + {"steamids": [steam_id] + friends_steam_ids}
+    params = auth_params | {"steamids": ",".join([steam_id] + friends_steam_ids)}
     response = httpx.get(endpoint, params=params)
+    content = response.json()
+    ## begin response format validation
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"failed to fetch summaries: {content}",
+        )
+
+    if "response" not in content or "players" not in content["response"]:
+        raise UnexpectedSteamAPIResponseFormatException
+    ## end response format validation
+
     summaries = response.json()["response"]["players"]
+
+    ## begin response format validation
+    for summary in summaries:
+        if (
+            "steamid" not in summary
+            or "profileurl" not in summary
+            or "personaname" not in summary
+            or "avatarfull" not in summary
+        ):
+            raise UnexpectedSteamAPIResponseFormatException
+    ## end response format validation
 
     # form friends DTOs
     friends = [
@@ -55,7 +123,10 @@ def get_user(slug: str):
     ]
 
     # form user DTO
-    summary = next(s for s in summaries if s["steamid"] == steam_id)
+    try:
+        summary = next(s for s in summaries if s["steamid"] == steam_id)
+    except:
+        raise UnexpectedSteamAPIResponseFormatException
     return SteamUser(
         user_slug=slug,
         user_url=summary["profileurl"],
