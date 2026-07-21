c100bd4094e7af28119f31723b2c620fd4e7e927
prettysorrow <prettysorrow44@gmail.com>
2026-07-21 21:38:02 +0300

    update project configs for python fetching
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- .secrets.example.sh
+++ .secrets.example.sh
@@ -1,3 +1,11 @@
 export POSTGRES_USER="..."
 export POSTGRES_PASSWORD="..."
 export POSTGRES_DB="..."
+
+export TELEGRAM_API_ID="..."
+export TELEGRAM_API_HASH="..."
+
+export INSTAGRAM_LOGIN="..."
+export INSTAGRAM_PASSWORD="..."
+
+export STEAM_WEB_API_KEY="..."
