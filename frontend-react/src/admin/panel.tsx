c95f482b25e7eca0c2e1d0af10c77083826856e3
prettysorrow <prettysorrow44@gmail.com>
2026-07-29 16:06:33 +0300

    add admin panel
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/admin/panel.tsx
@@ -0,0 +1,26 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { WebHoundUsers, WebHoundRequests } from "../components/entities/webhound";
+import { useWebHoundFetching } from "@/hooks/useFetching";
+
+export function WebHoundAdminPanel() {
+  let data = useWebHoundFetching();
+
+  return (
+    <Tabs>
+      <TabsList>
+        <TabsTrigger value="users">Users</TabsTrigger>
+        <TabsTrigger value="requests">Requests</TabsTrigger>
+        <TabsTrigger value="statistics">Statistics</TabsTrigger>
+      </TabsList>
+      <TabsContent value={"users"}>
+        <WebHoundUsers users={data.users} />
+      </TabsContent>
+      <TabsContent value={"requests"}>
+        <WebHoundRequests requests={data.requests} />
+      </TabsContent>
+      <TabsContent value={"statistics"}>
+        <div>some statistics</div>
+      </TabsContent>
+    </Tabs>
+  );
+}
