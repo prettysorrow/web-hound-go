c19e6df383e089c196164fd5f0bf43e7e272e2eb
prettysorrow <prettysorrow44@gmail.com>
2026-07-29 16:01:13 +0300

    minor renamings
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/transport/dtos/webhound.tsx
+++ frontend-react/src/transport/dtos/webhound.tsx
@@ -10,5 +10,10 @@ export interface WebHoundRequest {
   created_at: string;
   created_on: string;
   created_by: WebHoundUser;
-  results: { service: string; result: any };
+  results: { service: string; result: any }[];
+}
+
+export interface WebHoundDataToFetch {
+  users: WebHoundUser[];
+  requests: WebHoundRequest[];
 }
