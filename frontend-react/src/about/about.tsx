08003a71eefe693fe8f991422753bbefd18f11b3
prettysorrow <prettysorrow44@gmail.com>
2026-07-29 16:04:25 +0300

    add about page
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/about/about.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { GithubIcon, GoogleIcon, TelegramIcon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+
+export function WebHoundAbout() {
+  return (
+    <div
+      className="grid grid-rows-2 justify-items-center"
+      style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
+    >
+      <p className="text-5xl">Web Hound</p>
+      <p className="text-lg">Search, Seek, Destroy.</p>
+      <div className="flex">
+        <HugeiconsIcon icon={GoogleIcon} size="40px" />
+        <Button variant="ghost" className="text-[14px] w-auto h-auto px-2 py-3">
+          Login
+        </Button>
+        <span className="w-8"></span>
+        <HugeiconsIcon icon={TelegramIcon} size="40px" />
+        <Button variant="ghost" className="text-[14px] w-auto h-auto px-2 py-3">
+          Login
+        </Button>
+        <span className="w-8"></span>
+        <HugeiconsIcon icon={GithubIcon} size="40px" />
+        <Button variant="ghost" className="text-[14px] w-auto h-auto px-2 py-3">
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
