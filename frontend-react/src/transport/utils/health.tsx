0fec3bf1e35401271ab4ba84048cb9e3e3c90f89
prettysorrow <prettysorrow44@gmail.com>
2026-07-29 16:08:45 +0300

    add health util
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/transport/utils/health.tsx
@@ -0,0 +1,18 @@
+import FrontendEnvVars from "@/context/env";
+import axios from "axios";
+
+const base_url = FrontendEnvVars.VITE_BACKEND_API_URL;
+
+export async function GetBackendHealth(): Promise<{ kind: "ok" } | { kind: "err"; msg: string }> {
+  try {
+    const response = await axios.get(`${base_url}/health`);
+    if (response.status != 200) {
+      return { kind: "err", msg: response.statusText };
+    }
+
+    return { kind: "ok" };
+  } catch (error) {
+    console.error(`failed to connect to backend server: ${error}`);
+    return { kind: "err", msg: `${error}` };
+  }
+}
