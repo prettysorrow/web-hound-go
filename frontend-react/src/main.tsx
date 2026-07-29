35033b2605eb323cc5b347cb6424b9371e0528dd
prettysorrow <prettysorrow44@gmail.com>
2026-07-29 16:01:37 +0300

    formatting
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,13 +1,16 @@
-import './styles/index.css'
+import "./styles/index.css";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import { RouterProvider } from 'react-router-dom'
-import WebHoundRouter from './routing/router'
+import { RouterProvider } from "react-router-dom";
+import WebHoundRouter from "./routing/router";
+import { WebHoundFetchingProvider } from "./hooks/useFetching";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={WebHoundRouter} />
-  </StrictMode>
-)
+    <WebHoundFetchingProvider>
+      <RouterProvider router={WebHoundRouter} />
+    </WebHoundFetchingProvider>
+  </StrictMode>,
+);
