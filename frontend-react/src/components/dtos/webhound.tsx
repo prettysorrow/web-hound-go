cc55585b69f58b46bfb2aa50f1d45998773c2ca5
prettysorrow <prettysorrow44@gmail.com>
2026-07-29 05:19:23 +0300

    add webhound dto
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/components/dtos/webhound.tsx
@@ -0,0 +1,14 @@
+export type LoginServices = "telegram" | "gmail";
+
+export interface WebHoundUser {
+  used_service: LoginServices;
+  service_id: string;
+  display_name: string;
+}
+
+export interface WebHoundRequest {
+  created_at: string;
+  created_on: string;
+  created_by: WebHoundUser;
+  results: { service: string; result: any };
+}
