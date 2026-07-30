264ab6662cf060f116711c517c68135f08a45cf3
prettysorrow <prettysorrow44@gmail.com>
2026-07-31 02:54:05 +0300

    add navbar
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/routing/nav.tsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+export function WebHoundNav() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to="/about">About</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to="/search">Search</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Settings</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul>
+              <li>
+                <NavigationMenuLink>
+                  <Link to="/settings/profile">Profile</Link>
+                </NavigationMenuLink>
+              </li>
+              <li>
+                <NavigationMenuLink>
+                  <Link to="/settings/credentials">Credentials</Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Admin</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul>
+              <NavigationMenuLink>
+                <Link to="/admin/users">Users</Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink>
+                <Link to="/admin/requests">Requests</Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink>
+                <Link to="/admin/statistics">Statistics</Link>
+              </NavigationMenuLink>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
