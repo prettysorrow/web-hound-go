b461c011a2d07ad817a107446f4f99159fd7e8aa
prettysorrow <prettysorrow44@gmail.com>
2026-07-31 02:57:49 +0300

    add better searching results printings
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/components/entities/webhound.tsx
+++ frontend-react/src/components/entities/webhound.tsx
@@ -4,19 +4,20 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Badge } from "@/components/ui/badge";
 import { ShowServiceName, ShowServiceIcon } from "@/admin/services";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { GitHubUserVerbose } from "@/results/github";
 
 function WebHoundUser(props: { user: WebHoundUser }) {
   return (
     <Card className="flex p-4">
+      <div className="text-base">
+        <text className="font-semibold">Display Name: </text>
+        <text className="font-normal">{props.user.display_name}</text>
+      </div>
       <div className="flex gap-1 content-center">
         <Badge className="text-sm font-semibold" variant="outline">
           {props.user.used_service}
         </Badge>
         <ShowServiceIcon service={props.user.used_service} size="18px" />
-      </div>
-      <div className="text-lg">
-        <text className="font-semibold">Display Name: </text>
-        <text className="font-normal">{props.user.display_name}</text>
       </div>
       <div>
         <text className="font-semibold">{ShowServiceName(props.user.used_service)}: </text>
@@ -32,27 +33,44 @@ export function WebHoundUsers(props: { users: WebHoundUser[] }) {
   );
 }
 
+export function WebHoundResult(props: { service: string; result: any }) {
+  return (
+    <div>
+      <div className="text-lg font-semibold">Results for {props.service}</div>
+      <pre className="text-sm font-mono bg-gray-100 p-4 rounded overflow-auto">
+        {JSON.stringify(props.result, null, 2)}
+      </pre>
+    </div>
+  );
+}
+
+export function WebHoundResults(props: { results: { service: string; result: any }[] }) {
+  if (props.results.length === 0) {
+    return <div className="text-lg font-semibold">Empty results</div>;
+  }
+
+  return (
+    <div>
+      {props.results.map((result) => (
+        <WebHoundResult service={result.service} result={result.result} />
+      ))}
+    </div>
+  );
+}
+
 export function WebHoundRequest(props: { request: WebHoundRequest }) {
   return (
     <Card>
       <CardHeader>
         <CardTitle>Request on {props.request.created_on}</CardTitle>
         <CardDescription>
-          <div>
-            Created at {props.request.created_at} by {props.request.created_by.display_name}
-          </div>
-          <div>
-            Service: {ShowServiceName(props.request.created_by.used_service)}, Id:{" "}
-            {props.request.created_by.service_id}
-          </div>
+          <div>Created by {props.request.created_by.display_name}</div>
+          <div>Author service: {ShowServiceName(props.request.created_by.used_service)}</div>
+          <div>Author service id: {props.request.created_by.service_id}</div>
+          <div>Created at {props.request.created_at}</div>
         </CardDescription>
         <CardContent>
-          {props.request.results.map((request) => (
-            <div>
-              <div className="text-lg font-semibold">{request.service}</div>
-              <ScrollArea className="text-sm font-light">{request.result}</ScrollArea>
-            </div>
-          ))}
+          <WebHoundResults results={props.request.results} />
         </CardContent>
       </CardHeader>
     </Card>
@@ -62,7 +80,9 @@ export function WebHoundRequest(props: { request: WebHoundRequest }) {
 export function WebHoundRequests(props: { requests: WebHoundRequest[] }) {
   return (
     <div className="flex flex-col gap-2">
-      {props.requests.map((request) => WebHoundRequest({ request }))}
+      {props.requests.map((request) => (
+        <WebHoundRequest request={request} />
+      ))}
     </div>
   );
 }
---
+++ frontend-react/src/results/github.tsx
@@ -0,0 +1,47 @@
+import type { GitHubUser, GitHubUserShort, GitHubUserVerbose } from "@/transport/dtos/github";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+// representation of results for github
+
+export function GitHubUserShort(props: { username: string; pfp_url: string }) {
+  return (
+    <div className="flex gap-2">
+      <Avatar size="sm">
+        <AvatarImage src={props.pfp_url} />
+      </Avatar>
+      <span className="text-sm">{props.username}</span>
+    </div>
+  );
+}
+
+export function ScrollableGithubShortUsers(props: { users: GitHubUser[] }) {
+  return (
+    <div className="max-h-48 overflow-y-scroll">
+      {props.users.map((user) => GitHubUserShort(user))}
+    </div>
+  );
+}
+
+export function GitHubUserVerbose(props: {
+  kind: "verbose";
+  username: string;
+  pfp_url: string;
+  followees: GitHubUser[];
+  followers: GitHubUser[];
+}) {
+  <Card>
+    <CardHeader>
+      <Avatar>
+        <AvatarImage src={props.pfp_url} />
+      </Avatar>
+      <span className="text-lg">{props.username}</span>
+    </CardHeader>
+    <CardContent>
+      <h5>Followees</h5>
+      <ScrollableGithubShortUsers users={props.followees} />
+      <h5>Followers</h5>
+      <ScrollableGithubShortUsers users={props.followers} />
+    </CardContent>
+  </Card>;
+}
