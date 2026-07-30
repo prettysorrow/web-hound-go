e21b461c0a654e253f7614f55fd2804d61b9fe81
prettysorrow <prettysorrow44@gmail.com>
2026-07-31 02:58:06 +0300

    update routing
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/routing/App.tsx
+++ frontend-react/src/routing/App.tsx
@@ -1,15 +1,10 @@
-import { WebHoundAdminPanel } from "@/admin/panel";
-import { WebHoundSettingsDropdownMenu } from "@/settings/settings";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { WebHoundNav } from "./nav";
 
 function App() {
   return (
     <>
-      <nav style={{ display: "flex", justifyContent: "center", gap: "1%" }}>
-        <Link to="/about">About</Link>
-        <WebHoundSettingsDropdownMenu />
-        <WebHoundAdminPanel />
-      </nav>
+      <WebHoundNav />
       <Outlet />
     </>
   );
--- frontend-react/src/routing/router.tsx
+++ frontend-react/src/routing/router.tsx
@@ -1,8 +1,10 @@
 import App from "./App";
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import { WebHoundServices } from "@/settings/services";
 import { WebHoundCredentials } from "@/settings/credentials";
 import { WebHoundAbout } from "@/about/about";
+import { WebHoundSearch } from "@/seeking/search";
+import { WebHoundProfile } from "@/settings/profile";
+import { WebHoundRequestsMenu, WebHoundStatisticsMenu, WebHoundUsersMenu } from "@/admin/layouts";
 
 const WebHoundRouter = createBrowserRouter([
   {
@@ -14,6 +16,10 @@ const WebHoundRouter = createBrowserRouter([
         element: <WebHoundAbout />,
       },
       {
+        path: "search",
+        element: <WebHoundSearch />,
+      },
+      {
         path: "settings",
         children: [
           {
@@ -22,12 +28,34 @@ const WebHoundRouter = createBrowserRouter([
             element: <Navigate to="/" replace />,
           },
           {
-            path: "services",
-            element: <WebHoundServices />,
+            path: "profile",
+            element: <WebHoundProfile />,
           },
           {
             path: "credentials",
             element: <WebHoundCredentials />,
+          },
+        ],
+      },
+      {
+        path: "admin",
+        children: [
+          {
+            // redirect to root cuz admin does not have its own page
+            index: true,
+            element: <Navigate to="/" replace />,
+          },
+          {
+            path: "users",
+            element: <WebHoundUsersMenu />,
+          },
+          {
+            path: "requests",
+            element: <WebHoundRequestsMenu />,
+          },
+          {
+            path: "statistics",
+            element: <WebHoundStatisticsMenu />,
           },
         ],
       },
