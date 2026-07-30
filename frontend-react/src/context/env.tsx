dbd18eee9c599591913dac4238cd32fd7fe96d2e
prettysorrow <prettysorrow44@gmail.com>
2026-07-31 02:50:20 +0300

    configure env for ui testing
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/context/env.tsx
+++ frontend-react/src/context/env.tsx
@@ -1,4 +1,5 @@
 interface FrontendEnvVarsType {
+  VITE_USE_TESTING_DATA: boolean;
   VITE_BACKEND_API_URL: string;
 }
 
@@ -6,8 +7,13 @@ if (import.meta.env.VITE_BACKEND_API_URL === undefined) {
   throw new Error("VITE_BACKEND_API_URL is not specified in react frontend ");
 }
 
+if (import.meta.env.VITE_USE_TESTING_DATA === undefined) {
+  throw new Error("VITE_USE_TESTING_DATA is not specified in react frontend ");
+}
+
 const FrontendEnvVars: FrontendEnvVarsType = {
   VITE_BACKEND_API_URL: import.meta.env.VITE_BACKEND_API_URL,
+  VITE_USE_TESTING_DATA: import.meta.env.VITE_USE_TESTING_DATA === "true",
 };
 
 export default FrontendEnvVars;
