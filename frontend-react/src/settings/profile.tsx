c62a4c7c64a942f3a2e8a2d29cc97a2072953910
prettysorrow <prettysorrow44@gmail.com>
2026-07-31 02:56:33 +0300

    add profile tab
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/settings/profile.tsx
@@ -0,0 +1,24 @@
+import { ShowServiceIcon, ShowServiceName } from "@/admin/services";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { useWebhoundActiveUser } from "@/hooks/useWebHoundUser";
+
+export function WebHoundProfile() {
+  let user = useWebhoundActiveUser();
+
+  return (
+    <div className="w-full">
+      <div className="w-1/3 mx-auto">
+        <FieldGroup>
+          <Field>
+            <h1>Hello, {user.display_name}!</h1>
+          </Field>
+          <Field orientation={"horizontal"}>
+            <span>Used service:</span>
+            <ShowServiceIcon size="20px" service={user.used_service} />
+            <span>{ShowServiceName(user.used_service)}</span>
+          </Field>
+        </FieldGroup>
+      </div>
+    </div>
+  );
+}
