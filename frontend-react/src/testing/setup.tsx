e00e7a7b9f821fe227073c5d2fc41230037eb9b5
prettysorrow <prettysorrow44@gmail.com>
2026-08-03 23:34:36 +0300

    minor renaming
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/testing/setup.tsx
+++ frontend-react/src/testing/setup.tsx
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom";
+
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { setupServer } from "msw/node";
 import { WebHoundTesting___BackendHandlers } from "./mocks/webhound";
 
-export const server = setupServer(...WebHoundTesting___BackendHandlers);
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+export const backend = setupServer(...WebHoundTesting___BackendHandlers);
+beforeAll(() => backend.listen({ onUnhandledRequest: "error" }));
+afterEach(() => backend.resetHandlers());
+afterAll(() => backend.close());
