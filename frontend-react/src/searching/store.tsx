633ccd58ceb9aa667fa1c3b87193b2b67ff1e80c
prettysorrow <prettysorrow44@gmail.com>
2026-08-07 06:09:59 +0300

    add searching state store
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/searching/store.tsx
@@ -0,0 +1,36 @@
+import type { GitHubUserVerbose } from "@/transport/dtos/github";
+import type { InstagramUserVerbose } from "@/transport/dtos/instagram";
+import { create } from "zustand";
+import type { Searcher } from "./searcher";
+import { useSearcher } from "@/hooks/useSearcher";
+import { useWebHoundEnabledServices } from "@/settings/enabled-services/store";
+
+export type WebHoundStore___Searching = {
+  username: string | undefined;
+  github: GitHubUserVerbose | "Not found" | "Disabled" | undefined;
+  instagram: InstagramUserVerbose | "Not found" | "No credentials" | "Disabled" | undefined;
+  setUsername(username: string): void;
+  setGitHub(value: GitHubUserVerbose | "Not found" | "Disabled"): void;
+  setInstagram(user: InstagramUserVerbose | "Not found" | "No credentials" | "Disabled"): void;
+};
+
+export const useWebHoundSearchingStore = create<WebHoundStore___Searching>((set) => {
+  useWebHoundEnabledServices.subscribe((state, prev) => {
+    if (state.enabled.github !== prev.enabled.github && !state.enabled.github) {
+      set((state) => ({ ...state, github: "Disabled" }));
+    }
+
+    if (state.enabled.instagram !== prev.enabled.instagram && !state.enabled.instagram) {
+      set((state) => ({ ...state, instagram: "Disabled" }));
+    }
+  });
+
+  return {
+    username: undefined,
+    github: undefined,
+    instagram: undefined,
+    setUsername: (username) => set((state) => ({ ...state, username: username })),
+    setGitHub: (value) => set((state) => ({ ...state, github: value })),
+    setInstagram: (value) => set((state) => ({ ...state, instagram: value })),
+  };
+});
