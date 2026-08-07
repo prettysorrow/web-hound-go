d72de27f4cb4bec7fad27583a31eec4bd6b81046
prettysorrow <prettysorrow44@gmail.com>
2026-08-07 06:07:52 +0300

    provide searching settings layout
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/settings/searching.tsx
@@ -0,0 +1,9 @@
+import { WebHoundEnabledServices } from "./enabled-services/panel";
+
+export function WebHoundSearchingSettings() {
+  return (
+    <div>
+      <WebHoundEnabledServices />
+    </div>
+  );
+}
