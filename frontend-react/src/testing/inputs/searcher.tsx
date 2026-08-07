edd93c7e8adf0e53ddd4011ea3164177328e7423
prettysorrow <prettysorrow44@gmail.com>
2026-08-07 06:12:04 +0300

    simplify mock for searcher
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/testing/inputs/searcher.tsx
+++ frontend-react/src/testing/inputs/searcher.tsx
@@ -1,24 +1,14 @@
 import type { Searcher } from "@/searching/searcher";
 import type { GitHubUserVerbose } from "@/transport/dtos/github";
 import { WebHoundTesting___GitHubUsers } from "./github-users";
-import type { EnabledServicesType } from "@/searching/enabled-services";
+import type { InstagramUserVerbose } from "@/transport/dtos/instagram";
+import { WebHoundTesting___InstagramUsers } from "./instagram-users";
 
 export class WebHoundTesting___Searcher implements Searcher {
-  private _enabledServices: EnabledServicesType;
   private _isLoading: boolean;
 
-  constructor(enabledServices: EnabledServicesType) {
+  constructor() {
     this._isLoading = false;
-    this._enabledServices = enabledServices;
-    if (this._enabledServices.instagram) {
-      throw new Error("not implemented: searcher for instagram");
-    }
-    if (this._enabledServices.steam) {
-      throw new Error("not implemented: searcher for steam");
-    }
-    if (this._enabledServices.telegram) {
-      throw new Error("not implemented: searcher for telegram");
-    }
   }
 
   get isLoading(): boolean {
@@ -26,12 +16,10 @@ export class WebHoundTesting___Searcher implements Searcher {
   }
 
   public searchGitHub(username: string): GitHubUserVerbose | undefined {
-    if (this._enabledServices.github) {
-      return WebHoundTesting___GitHubUsers.find((user) => user.username === username);
-    } else {
-      throw new Error(
-        "invalid operation: github seaching is not available for this instance of searcher",
-      );
-    }
+    return WebHoundTesting___GitHubUsers.find((user) => user.username === username);
+  }
+
+  public searchInstagram(username: string): InstagramUserVerbose | undefined {
+    return WebHoundTesting___InstagramUsers.find((user) => user.username === username);
   }
 }
