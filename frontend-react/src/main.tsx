a346e3ef0787b92d840e94eb82221c051b11d33f
prettysorrow <prettysorrow44@gmail.com>
2026-08-07 06:22:05 +0300

    add searcher provider to main
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -7,12 +7,15 @@ import { RouterProvider } from "react-router-dom";
 import WebHoundRouter from "./routing/router";
 import { WebHoundFetchingProvider } from "./hooks/WebHoundFetchingProvider";
 import { WithCredentials } from "./hooks/useCredentials";
+import { WithSearcher } from "./hooks/useSearcher";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WebHoundFetchingProvider>
       <WithCredentials>
-        <RouterProvider router={WebHoundRouter} />
+        <WithSearcher>
+          <RouterProvider router={WebHoundRouter} />
+        </WithSearcher>
       </WithCredentials>
     </WebHoundFetchingProvider>
   </StrictMode>,
