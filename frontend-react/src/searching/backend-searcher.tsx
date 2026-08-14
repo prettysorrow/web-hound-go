85712421141472a06a2acc1f41d2bda0f0fc3f88
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 09:21:02 +0300

    add backend searcher
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ frontend-react/src/searching/backend-searcher.tsx
@@ -0,0 +1,29 @@
+import type { Searcher } from "@/searching/searcher";
+import { GetGitHubUser } from "@/transport/fetching/github";
+import { GetInstagramUser } from "@/transport/fetching/instagram";
+import type { GitHubUserVerbose } from "@/transport/dtos/github";
+import type { InstagramUserVerbose } from "@/transport/dtos/instagram";
+
+export class WebHoundBackend___Searcher implements Searcher {
+  private _isLoading: boolean;
+
+  constructor() {
+    this._isLoading = false;
+  }
+
+  get isLoading(): boolean {
+    return this._isLoading;
+  }
+
+  get requiresInstagramCredentials(): boolean {
+    return false;
+  }
+
+  public async searchGitHub(username: string): Promise<GitHubUserVerbose | undefined> {
+    return GetGitHubUser(username);
+  }
+
+  public async searchInstagram(username: string): Promise<InstagramUserVerbose | undefined> {
+    return GetInstagramUser(username);
+  }
+}
