33a6943c7da4097eac55ef678f226847b9c5c269
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 08:16:20 +0300

    add instagram dtos
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ entities/instagram/transport/dtos.go
@@ -0,0 +1,21 @@
+package webhound_instagram_transport
+
+type InstagramUserShort struct {
+	Kind     string `json:"kind"`
+	Username string `json:"username"`
+	PfpUrl   string `json:"pfp_url"`
+}
+
+type InstagramUserPrivate struct {
+	Kind     string `json:"kind"`
+	Username string `json:"username"`
+	PfpUrl   string `json:"pfp_url"`
+}
+
+type InstagramUserPublic struct {
+	Kind      string               `json:"kind"`
+	Username  string               `json:"username"`
+	PfpUrl    string               `json:"pfp_url"`
+	Followees []InstagramUserShort `json:"followees"`
+	Followers []InstagramUserShort `json:"followers"`
+}
