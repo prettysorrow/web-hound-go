e1aaca2c966f487930cda2858ab5ed62945728bb
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 12:02:53 +0300

    improve error handling in fetching script
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- fetching/main.py
+++ fetching/main.py
@@ -16,27 +16,32 @@ with open(settings_path, "r") as f:
     settings = json.load(f)
 
     for service in settings["enabled-services"]:
-        match service:
-            case "steam":
-                from steam.handlers import router as steam_router
+        try:
+            match service:
+                case "steam":
+                    from steam.handlers import router as steam_router
 
-                app.include_router(steam_router)
-            case "github":
-                from github.handlers import router as github_router
+                    app.include_router(steam_router)
+                case "github":
+                    from github.handlers import router as github_router
 
-                app.include_router(github_router)
-            case "telegram":
-                from telegram.handlers import router as telegram_router
+                    app.include_router(github_router)
+                case "telegram":
+                    from telegram.handlers import router as telegram_router
 
-                app.include_router(telegram_router)
-            case "instagram":
-                from instagram.handlers import router as instagram_router
+                    app.include_router(telegram_router)
+                case "instagram":
+                    from instagram.handlers import router as instagram_router
 
-                app.include_router(instagram_router)
-            case other:
-                logging.warning(
-                    msg=f"unknown service specified in settings: {other} (ignoring)"
-                )
+                    app.include_router(instagram_router)
+                case other:
+                    logging.warning(
+                        msg=f"unknown service specified in settings: {other} (ignoring)"
+                    )
+        except Exception as ex:
+            logging.warning(
+                msg=f"failed to enable service {service}: {ex} (skipping)"
+            )
 
     if "server-host" in settings:
         host = settings["server-host"]
