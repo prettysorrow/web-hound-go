a603580d81f680cca8b3fbb5dcba0f572fa6eaa1
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 09:14:41 +0300

    replace pgx conn with pgx pool
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- database/postgres.go
+++ database/postgres.go
@@ -5,17 +5,22 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func Connect(connection_uri string) (*pgx.Conn, error) {
-	conn, err := pgx.Connect(context.Background(), connection_uri)
+func Connect(connection_uri string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), connection_uri)
 	if err != nil {
 		err = fmt.Errorf("failed to connect to database: %w", err)
 		return nil, err
 	}
 
-	return conn, nil
+	if err := pool.Ping(context.Background()); err != nil {
+		err = fmt.Errorf("failed to ping database: %w", err)
+		return nil, err
+	}
+
+	return pool, nil
 }
 
 func GetConnectionStringFromEnv() (*string, error) {
