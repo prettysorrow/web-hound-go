270167d70b05b576280d8433c3b3840e34bc6402
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 08:16:12 +0300

    add instagram database queries
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ entities/instagram/database/queries.go
@@ -0,0 +1,110 @@
+package webhound_instagram_database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type InsertInstagramUserInput struct {
+	Kind     string
+	Username string
+	PfpUrl   string
+}
+
+func InsertInstagramUser(db *pgx.Conn, ctx context.Context, user *InsertInstagramUserInput) (*InstagramUser, error) {
+	var id int64
+	row := db.QueryRow(ctx,
+		`insert into instagram."user" (kind, username, pfp_url)
+		values ($1, $2, $3)
+		on conflict (username)
+		do update set
+			kind = excluded.kind,
+			pfp_url = excluded.pfp_url
+		returning id;`,
+		user.Kind, user.Username, user.PfpUrl)
+	if err := row.Scan(&id); err != nil {
+		return nil, fmt.Errorf("failed to insert instagram user: %w", err)
+	}
+
+	return &InstagramUser{Id: id, Kind: user.Kind, Username: user.Username, PfpUrl: user.PfpUrl}, nil
+}
+
+func SelectInstagramUserByUsername(db *pgx.Conn, ctx context.Context, username string) (*InstagramUser, error) {
+	var user InstagramUser
+	row := db.QueryRow(ctx, "select id, kind, username, pfp_url from instagram.user where username = $1;", username)
+	if err := row.Scan(&user.Id, &user.Kind, &user.Username, &user.PfpUrl); err != nil {
+		return nil, fmt.Errorf("failed to select instagram user: %w", err)
+	}
+
+	return &user, nil
+}
+
+func SelectInstagramUserById(db *pgx.Conn, ctx context.Context, id int64) (*InstagramUser, error) {
+	var user InstagramUser
+	row := db.QueryRow(ctx, "select id, kind, username, pfp_url from instagram.user where id = $1;", id)
+	if err := row.Scan(&user.Id, &user.Kind, &user.Username, &user.PfpUrl); err != nil {
+		return nil, fmt.Errorf("failed to select instagram user: %w", err)
+	}
+
+	return &user, nil
+}
+
+type InsertInstagramFollowsInput struct {
+	FolloweeId int64
+	FollowerId int64
+}
+
+func InsertInstagramFollows(db *pgx.Conn, ctx context.Context, input *InsertInstagramFollowsInput) error {
+	_, err := db.Exec(ctx, "insert into instagram.follows (followee_id, follower_id) values ($1, $2);", input.FolloweeId, input.FollowerId)
+	if err != nil {
+		return fmt.Errorf("failed to insert instagram follows: %w", err)
+	}
+
+	return nil
+}
+
+func SelectInstagramFollowees(db *pgx.Conn, ctx context.Context, id int64) ([]InstagramUser, error) {
+	followees := []InstagramUser{}
+	rows, err := db.Query(ctx, "select user.id, user.kind, user.username, user.pfp_url from instagram.follows left join instagram.user on follows.followee_id = user.id where follows.follower_id = $1;", id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to select instagram followees: %w", err)
+	}
+
+	for rows.Next() {
+		var user InstagramUser
+		if err := rows.Scan(&user.Id, &user.Kind, &user.Username, &user.PfpUrl); err != nil {
+			return nil, fmt.Errorf("failed to select instagram followees: failed to scan a single row: %w", err)
+		}
+		followees = append(followees, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("failed to select instagram followees: failed to scan rows: %w", err)
+	}
+
+	return followees, nil
+}
+
+func SelectInstagramFollowers(db *pgx.Conn, ctx context.Context, id int64) ([]InstagramUser, error) {
+	followers := []InstagramUser{}
+	rows, err := db.Query(ctx, "select user.id, user.kind, user.username, user.pfp_url from instagram.follows left join instagram.user on follows.follower_id = user.id where follows.followee_id = $1;", id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to select instagram followers: %w", err)
+	}
+
+	for rows.Next() {
+		var user InstagramUser
+		if err := rows.Scan(&user.Id, &user.Kind, &user.Username, &user.PfpUrl); err != nil {
+			return nil, fmt.Errorf("failed to select instagram followers: failed to scan a single row: %w", err)
+		}
+		followers = append(followers, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("failed to select instagram followers: failed to scan rows: %w", err)
+	}
+
+	return followers, nil
+}
