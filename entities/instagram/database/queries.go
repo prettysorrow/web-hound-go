fcd7d0c1f9fb1bffee049be83840a90062be3d0d
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 09:16:33 +0300

    add instagram media database queries
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- entities/instagram/database/queries.go
+++ entities/instagram/database/queries.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type InsertInstagramUserInput struct {
@@ -13,7 +13,7 @@ type InsertInstagramUserInput struct {
 	PfpUrl   string
 }
 
-func InsertInstagramUser(db *pgx.Conn, ctx context.Context, user *InsertInstagramUserInput) (*InstagramUser, error) {
+func InsertInstagramUser(db *pgxpool.Pool, ctx context.Context, user *InsertInstagramUserInput) (*InstagramUser, error) {
 	var id int64
 	row := db.QueryRow(ctx,
 		`insert into instagram."user" (kind, username, pfp_url)
@@ -31,7 +31,7 @@ func InsertInstagramUser(db *pgx.Conn, ctx context.Context, user *InsertInstagra
 	return &InstagramUser{Id: id, Kind: user.Kind, Username: user.Username, PfpUrl: user.PfpUrl}, nil
 }
 
-func SelectInstagramUserByUsername(db *pgx.Conn, ctx context.Context, username string) (*InstagramUser, error) {
+func SelectInstagramUserByUsername(db *pgxpool.Pool, ctx context.Context, username string) (*InstagramUser, error) {
 	var user InstagramUser
 	row := db.QueryRow(ctx, "select id, kind, username, pfp_url from instagram.user where username = $1;", username)
 	if err := row.Scan(&user.Id, &user.Kind, &user.Username, &user.PfpUrl); err != nil {
@@ -41,7 +41,7 @@ func SelectInstagramUserByUsername(db *pgx.Conn, ctx context.Context, username s
 	return &user, nil
 }
 
-func SelectInstagramUserById(db *pgx.Conn, ctx context.Context, id int64) (*InstagramUser, error) {
+func SelectInstagramUserById(db *pgxpool.Pool, ctx context.Context, id int64) (*InstagramUser, error) {
 	var user InstagramUser
 	row := db.QueryRow(ctx, "select id, kind, username, pfp_url from instagram.user where id = $1;", id)
 	if err := row.Scan(&user.Id, &user.Kind, &user.Username, &user.PfpUrl); err != nil {
@@ -56,7 +56,7 @@ type InsertInstagramFollowsInput struct {
 	FollowerId int64
 }
 
-func InsertInstagramFollows(db *pgx.Conn, ctx context.Context, input *InsertInstagramFollowsInput) error {
+func InsertInstagramFollows(db *pgxpool.Pool, ctx context.Context, input *InsertInstagramFollowsInput) error {
 	_, err := db.Exec(ctx, "insert into instagram.follows (followee_id, follower_id) values ($1, $2);", input.FolloweeId, input.FollowerId)
 	if err != nil {
 		return fmt.Errorf("failed to insert instagram follows: %w", err)
@@ -65,9 +65,9 @@ func InsertInstagramFollows(db *pgx.Conn, ctx context.Context, input *InsertInst
 	return nil
 }
 
-func SelectInstagramFollowees(db *pgx.Conn, ctx context.Context, id int64) ([]InstagramUser, error) {
+func SelectInstagramFollowees(db *pgxpool.Pool, ctx context.Context, id int64) ([]InstagramUser, error) {
 	followees := []InstagramUser{}
-	rows, err := db.Query(ctx, "select user.id, user.kind, user.username, user.pfp_url from instagram.follows left join instagram.user on follows.followee_id = user.id where follows.follower_id = $1;", id)
+	rows, err := db.Query(ctx, `select u.id, u.kind, u.username, u.pfp_url from instagram.follows left join instagram."user" u on follows.followee_id = u.id where follows.follower_id = $1;`, id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to select instagram followees: %w", err)
 	}
@@ -87,9 +87,9 @@ func SelectInstagramFollowees(db *pgx.Conn, ctx context.Context, id int64) ([]In
 	return followees, nil
 }
 
-func SelectInstagramFollowers(db *pgx.Conn, ctx context.Context, id int64) ([]InstagramUser, error) {
+func SelectInstagramFollowers(db *pgxpool.Pool, ctx context.Context, id int64) ([]InstagramUser, error) {
 	followers := []InstagramUser{}
-	rows, err := db.Query(ctx, "select user.id, user.kind, user.username, user.pfp_url from instagram.follows left join instagram.user on follows.follower_id = user.id where follows.followee_id = $1;", id)
+	rows, err := db.Query(ctx, `select u.id, u.kind, u.username, u.pfp_url from instagram.follows left join instagram."user" u on follows.follower_id = u.id where follows.followee_id = $1;`, id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to select instagram followers: %w", err)
 	}
@@ -107,4 +107,48 @@ func SelectInstagramFollowers(db *pgx.Conn, ctx context.Context, id int64) ([]In
 	}
 
 	return followers, nil
+}
+
+func SelectInstagramPostsByUserId(db *pgxpool.Pool, ctx context.Context, id int64) ([]InstagramPost, error) {
+	posts := []InstagramPost{}
+	rows, err := db.Query(ctx, "select id, user_id, description from instagram.post where user_id = $1 order by id;", id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to select instagram posts: %w", err)
+	}
+
+	for rows.Next() {
+		var post InstagramPost
+		if err := rows.Scan(&post.Id, &post.UserId, &post.Description); err != nil {
+			return nil, fmt.Errorf("failed to select instagram posts: failed to scan a single row: %w", err)
+		}
+		posts = append(posts, post)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("failed to select instagram posts: failed to scan rows: %w", err)
+	}
+
+	return posts, nil
+}
+
+func SelectInstagramMediaByPostId(db *pgxpool.Pool, ctx context.Context, postId int64) ([]InstagramMedia, error) {
+	media := []InstagramMedia{}
+	rows, err := db.Query(ctx, "select id, post_id, kind, url from instagram.media where post_id = $1 order by id;", postId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to select instagram media: %w", err)
+	}
+
+	for rows.Next() {
+		var m InstagramMedia
+		if err := rows.Scan(&m.Id, &m.PostId, &m.Kind, &m.Url); err != nil {
+			return nil, fmt.Errorf("failed to select instagram media: failed to scan a single row: %w", err)
+		}
+		media = append(media, m)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("failed to select instagram media: failed to scan rows: %w", err)
+	}
+
+	return media, nil
 }
