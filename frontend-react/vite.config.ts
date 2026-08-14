4b996e5f59b7af2dd326bfe1c4a38f736e964430
prettysorrow <prettysorrow44@gmail.com>
2026-08-14 08:06:33 +0300

    fix ports in configs
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 5174,
+    strictPort: true,
+  },
   test: {
     globals: true,
     environment: "jsdom",
